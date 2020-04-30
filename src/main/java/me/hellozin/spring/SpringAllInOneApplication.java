49b8ffa6d33f9b6f3f398c3bebb54a4edb4c0cad
hellozin <paul26375@gmail.com>
2020-04-30 17:52:03 +0900

    Initial Commit

---
+++ src/main/java/me/hellozin/spring/SpringAllInOneApplication.java
@@ -0,0 +1,13 @@
+package me.hellozin.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllInOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAllInOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hellozin/spring/SpringAllInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hellozin.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllInOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
